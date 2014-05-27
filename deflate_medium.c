99a86daf5fb8941426766fb4715594fb08aabda4
Jim Kukunas <james.t.kukunas@linux.intel.com>
2014-05-27 10:08:47 -0700

    check whether match or orig go too far back
    Merges branch issue3.

--- deflate_medium.c
+++ deflate_medium.c
@@ -116,6 +116,12 @@ static void fizzle_matches(deflate_state *s, struct match *current, struct match
     if (current->match_length <= 1)
             return;
 
+    if (zunlikely(current->match_length > 1 + next->match_start))
+        return;
+
+    if (zunlikely(current->match_length > 1 + next->strstart))
+        return;
+
     match = s->window - current->match_length + 1 + next->match_start ;
     orig  = s->window - current->match_length + 1 + next->strstart ;
     
