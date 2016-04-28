1d227a260fcc2d7cce4198b979a5c80e3614b8d4
Hans Kristian Rosbach <hk-git@circlestorm.org>
2016-04-28 12:31:35 +0200

    Fix spelling of __DragonFly__ in crc32.c, found by @nemequ

--- crc32.c
+++ crc32.c
@@ -23,7 +23,7 @@
 # endif
 #elif __APPLE__
 # include <machine/endian.h>
-#elif defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__bsdi__) || defined(__DragoFly__)
+#elif defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__bsdi__) || defined(__DragonFly__)
 # include <sys/endian.h>
 #else
 # include <endian.h>
