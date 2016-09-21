2dd8ad328a7f52ad432b0cb752ae5d36798f4fbe
Mark Adler <madler@alumni.caltech.edu>
2016-09-20 17:27:28 -0700

    Correct the size of the inflate state in the comments.

--- inflate.h
+++ inflate.h
@@ -80,7 +80,8 @@ typedef enum {
         CHECK -> LENGTH -> DONE
  */
 
-/* state maintained between inflate() calls.  Approximately 10K bytes. */
+/* State maintained between inflate() calls -- approximately 7K bytes, not
+   including the allocated sliding window, which is up to 32K bytes. */
 struct inflate_state {
     inflate_mode mode;          /* current inflate mode */
     int last;                   /* true if processing last block */
