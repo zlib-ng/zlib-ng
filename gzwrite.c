4135626a9a931fc0ca3e4b716879d684358844f8
Mark Adler <madler@alumni.caltech.edu>
2017-01-02 16:17:43 -0800

    Fix bug in gzwrite.c that produced corrupt gzip files.

--- gzwrite.c
+++ gzwrite.c
@@ -103,6 +103,7 @@ static int gz_comp(gz_statep state, int flush) {
             if (strm->avail_out == 0) {
                 strm->avail_out = state->size;
                 strm->next_out = state->out;
+                state->x.next = state->out;
             }
             state->x.next = strm->next_out;
         }
