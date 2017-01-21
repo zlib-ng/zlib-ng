476be2432db3c88a7a2345fba32ee31e58903011
Mark Adler <madler@alumni.caltech.edu>
2017-01-21 12:13:25 -0800

    Fix bug when window full in deflate_stored().

--- deflate.c
+++ deflate.c
@@ -1478,7 +1478,7 @@ static block_state deflate_stored(deflate_state *s, int flush) {
         return block_done;
 
     /* Fill the window with any remaining input. */
-    have = s->window_size - s->strstart - 1;
+    have = s->window_size - s->strstart;
     if (s->strm->avail_in > have && s->block_start >= (long)s->w_size) {
         /* Slide the window down. */
         s->block_start -= s->w_size;
