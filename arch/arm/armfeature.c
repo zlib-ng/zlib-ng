77991bb4128176be959ad63c86e3a6e09d206703
Mika Lindqvist <postmaster@raasu.org>
2018-03-13 11:26:19 +0200

    [ARM/AArch64] Add run-time detection of ACLE and NEON instructions under Linux. * Use getauxval() to check support for ACLE CRC32 instructions * Allow disabling CRC32 instruction check

---
+++ arch/arm/armfeature.c
@@ -0,0 +1,33 @@
+#if defined(__linux__)
+# include <sys/auxv.h>
+# include <asm/hwcap.h>
+#elif defined(_WIN32)
+# include <winapifamily.h>
+#endif
+
+int arm_has_crc32() {
+#if defined(__linux__) && defined(HWCAP2_CRC32)
+  return (getauxval(AT_HWCAP2) & HWCAP2_CRC32) != 0 ? 1 : 0;
+#elif defined(ARM_NOCHECK_ACLE)
+  return 1;
+#else
+  return 0;
+#endif
+}
+
+int arm_has_neon()
+{
+#if defined(__linux__) && defined(HWCAP_NEON)
+  return (getauxval(AT_HWCAP) & HWCAP_NEON) != 0 ? 1 : 0;
+#elif defined(_M_ARM) && defined(WINAPI_FAMILY_PARTITION)
+# if WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_PHONE_APP)
+  return 1; /* Always supported */
+# endif
+#endif
+
+#if defined(ARM_NOCHECK_NEON)
+  return 1;
+#else
+  return 0;
+#endif
+}
