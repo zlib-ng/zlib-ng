2d683ceba8a4af73afa26527e19f095d5c5fe1e6
Mark Adler <madler@alumni.caltech.edu>
2018-04-17 22:44:41 -0700

    Assure that the number of bits for deflatePrime() is valid.

--- deflate.c
+++ deflate.c
@@ -553,7 +553,8 @@ int ZEXPORT PREFIX(deflatePrime)(PREFIX3(stream) *strm, int bits, int value) {
     if (deflateStateCheck(strm))
         return Z_STREAM_ERROR;
     s = strm->state;
-    if (s->sym_buf < s->pending_out + ((Buf_size + 7) >> 3))
+    if (bits < 0 || bits > 16 ||
+        s->sym_buf < s->pending_out + ((Buf_size + 7) >> 3))
         return Z_BUF_ERROR;
     do {
         put = Buf_size - s->bi_valid;
