cfbf2a60d109562aa7cf759d22350082af757222
Sebastian Pop <s.pop@samsung.com>
2018-08-10 23:30:00 -0500

    make LLVMFuzzerTestOneInput return 0

--- test/fuzz/compress_fuzzer.c
+++ test/fuzz/compress_fuzzer.c
@@ -1,6 +1,8 @@
 #include <stddef.h>
 #include <stdint.h>
 #include <string.h>
+#include <assert.h>
+#include <stdlib.h>
 
 #include "zbuild.h"
 #ifdef ZLIB_COMPAT
@@ -9,16 +11,20 @@
 #  include "zlib-ng.h"
 #endif
 
-static uint8_t cbuf[256 * 1024] = {0};
-static uint8_t ubuf[256 * 1024] = {0};
-
 int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
-  size_t cbuf_len = sizeof(cbuf);
-  size_t ubuf_len = sizeof(ubuf);
+  uint8_t *cbuf = (uint8_t *)malloc(size);
+  uint8_t *ubuf = (uint8_t *)malloc(size);
+  size_t cbuf_len = size, ubuf_len = size;
   PREFIX(compress)(cbuf, &cbuf_len, data, size);
   PREFIX(uncompress)(ubuf, &ubuf_len, cbuf, cbuf_len);
-  if (ubuf_len != size)
-    return 1;
 
-  return memcmp(data, ubuf, ubuf_len);
+  /* Make sure compress + uncompress gives back the input data. */
+  assert(size == ubuf_len);
+  assert(0 == memcmp(data, ubuf, size));
+
+  free(cbuf);
+  free(ubuf);
+
+  /* This function must return 0. */
+  return 0;
 }
