a74a9382989ad7af5f683a6070d1ea58d9530412
Sebastian Pop <s.pop@samsung.com>
2018-08-15 13:40:34 -0500

    compress_fuzzer: check different compression levels

--- test/fuzz/compress_fuzzer.c
+++ test/fuzz/compress_fuzzer.c
@@ -11,16 +11,27 @@
 #  include "zlib-ng.h"
 #endif
 
-int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
-  size_t cbuf_len = 100 + 2 * size, ubuf_len = size;
-  uint8_t *cbuf = (uint8_t *)malloc(cbuf_len);
-  uint8_t *ubuf = (uint8_t *)malloc(ubuf_len);
-  PREFIX(compress)(cbuf, &cbuf_len, data, size);
+static void check_compress_level(const uint8_t *data, size_t size,
+                                 uint8_t *cbuf, size_t cbuf_len,
+                                 uint8_t *ubuf, size_t ubuf_len,
+                                 int level) {
+  PREFIX(compress2)(cbuf, &cbuf_len, data, size, level);
   PREFIX(uncompress)(ubuf, &ubuf_len, cbuf, cbuf_len);
 
   /* Make sure compress + uncompress gives back the input data. */
   assert(size == ubuf_len);
   assert(0 == memcmp(data, ubuf, size));
+}
+
+int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+  size_t cbuf_len = 100 + 2 * size, ubuf_len = size;
+  uint8_t *cbuf = (uint8_t *)malloc(cbuf_len);
+  uint8_t *ubuf = (uint8_t *)malloc(ubuf_len);
+
+  check_compress_level(data, size, cbuf, cbuf_len, ubuf, ubuf_len, 1);
+  check_compress_level(data, size, cbuf, cbuf_len, ubuf, ubuf_len, 3);
+  check_compress_level(data, size, cbuf, cbuf_len, ubuf, ubuf_len, 6);
+  check_compress_level(data, size, cbuf, cbuf_len, ubuf, ubuf_len, 7);
 
   free(cbuf);
   free(ubuf);
