fbcae4a10123cbf6e491df576df24e72c2475bf3
Sebastian Pop <s.pop@samsung.com>
2018-08-17 09:13:05 -0500

    address review comments
    https://github.com/sebpop/zlib-ng/commit/5b5860b7092dc084e15884bc357c9253c72ca3c0#commitcomment-30131656
    > Compression level and window size are not really needed for decompression code,
    > IIRC they are left there as metadata for information purposes only, so that a
    > quick inspection of a file/stream can see whether it might be a good idea to
    > re-compress it with a higher compression level.  Because of that, I doubt there
    > is much/any gain by running the decompression code for each of those
    > combinations. I suspect it will only be a waste of cpu time.
    
    To speed up the fuzzer code, remove memcpy of the input data, and call
    check_decompress once with no preset dictionary.

--- test/fuzz/compress_fuzzer.c
+++ test/fuzz/compress_fuzzer.c
@@ -26,8 +26,9 @@ static void check_compress_level(const uint8_t *data, size_t size,
 #define put_byte(s, i, c) {s[i] = (unsigned char)(c);}
 #define PRESET_DICT 0x20 /* preset dictionary flag in zlib header */
 
-static void write_zlib_header(uint8_t *s, unsigned level_flags, unsigned w_bits,
-                              unsigned preset_dict) {
+static void write_zlib_header(uint8_t *s, unsigned preset_dict) {
+  unsigned level_flags = 0; /* compression level (0..3) */
+  unsigned w_bits = 8; /* window size log2(w_size)  (8..16) */
   unsigned int header = (Z_DEFLATED + ((w_bits-8)<<4)) << 8;
   header |= (level_flags << 6);
 
@@ -40,42 +41,27 @@ static void write_zlib_header(uint8_t *s, unsigned level_flags, unsigned w_bits,
   put_byte(s, 1, (unsigned char)(header & 0xff));
 }
 
-static void check_decompress(uint8_t *cbuf, size_t cbuf_len, uint8_t *dbuf,
-                             size_t dbuf_len) {
-  /* Write a gzip header for each level and for each window size and try
-     decompress the random input data. */
-  unsigned level_flags = 0; /* compression level (0..3) */
-  for (; level_flags < 4; ++level_flags) {
-    unsigned w_bits = 8; /* window size log2(w_size)  (8..16) */
-    for (; w_bits <= 16; ++w_bits) {
-      unsigned preset_dict = 0; /* (0..1) */
-      for (; preset_dict <= 1; ++preset_dict) {
-        write_zlib_header(dbuf, level_flags, w_bits, preset_dict);
-        PREFIX(uncompress)(cbuf, &cbuf_len, dbuf, dbuf_len);
-      }
-    }
-  }
+/* No preset dictionary. */
+static void check_decompress_no_dict(uint8_t *cbuf, size_t cbuf_len,
+                                     uint8_t *data, size_t data_len) {
+  write_zlib_header(data, 0);
+  PREFIX(uncompress)(cbuf, &cbuf_len, data, data_len);
 }
 
 int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
   size_t cbuf_len = 100 + 2 * size, ubuf_len = size;
-  /* Need at least two bytes for the gzip header. */
-  size_t dbuf_len = size + 2;
   uint8_t *cbuf = (uint8_t *)malloc(cbuf_len);
   uint8_t *ubuf = (uint8_t *)malloc(ubuf_len);
-  uint8_t *dbuf = (uint8_t *)malloc(dbuf_len);
 
   check_compress_level(data, size, cbuf, cbuf_len, ubuf, ubuf_len, 1);
   check_compress_level(data, size, cbuf, cbuf_len, ubuf, ubuf_len, 3);
   check_compress_level(data, size, cbuf, cbuf_len, ubuf, ubuf_len, 6);
   check_compress_level(data, size, cbuf, cbuf_len, ubuf, ubuf_len, 7);
 
-  /* Copy the input data after the first two bytes left for the gzip header. */
-  memcpy(dbuf + 2, data, size);
-  check_decompress(cbuf, cbuf_len, dbuf, dbuf_len);
-  check_decompress(cbuf, size, dbuf, dbuf_len);
-  check_decompress(cbuf, 100, dbuf, dbuf_len);
-  check_decompress(cbuf, 10, dbuf, dbuf_len);
+  /* check_decompress will write two bytes for the zlib header: check that the
+     buffer is long enough and cast const away from data. */
+  if (size > 2)
+    check_decompress_no_dict(cbuf, cbuf_len, (uint8_t *)data, size);
 
   free(cbuf);
   free(ubuf);
