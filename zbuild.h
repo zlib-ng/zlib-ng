0b565ced5a10cc829349134e5754c4d866093d0d
Alexey Milovidov <milovidov@yandex-team.ru>
2018-08-24 02:15:09 +0300

    Revert "Fixed build"
    This reverts commit 9173b89d46799582d20a30578e0aa9788bc7d6e1.

--- zbuild.h
+++ zbuild.h
@@ -12,7 +12,7 @@
 #  define PREFIX2(x) ZLIB_ ## x
 #  define PREFIX3(x) z_ ## x
 #  define zVersion zlibVersion
-#  define z_size_t size_t
+#  define z_size_t unsigned long
 #else
 #  define PREFIX(x) zng_ ## x
 #  define PREFIX2(x) ZLIBNG_ ## x
