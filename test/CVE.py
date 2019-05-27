3672ff102f384dc6e44b7e4273bb74058a21de44
Nathan Moinvaziri <nathan@solidstatenetworks.com>
2019-05-26 23:07:16 -0700

    Added support for cross-compiling with ARM in cmake. Fixed bug detecting float abi in cmake. Added cross-platform testing of CVEs with python. Simplified the generation of fuzzer exes in cmake. Updated travis CI to use cmake only. Added windows os to the matrix list in travis CI. Cleanup cmake a bit.

---
+++ test/CVE.py
@@ -0,0 +1,22 @@
+import sys
+import subprocess
+
+def main():
+    cmd = sys.argv[2:]
+    proc = subprocess.Popen(cmd,
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.STDOUT)
+    stdout, stderr = proc.communicate()
+    print stdout
+    #print stderr
+    # We expect 1 error code is OK and we expect 134 or 
+    # similar exit code for a vulnerable failure
+    if proc.returncode == 1 or proc.returncode == 0:
+        print('zlib is not vulnerable to {0} ({1})'.format(sys.argv[1], proc.returncode))
+        exit(0)
+    else:
+        print('zlib VULNERABLE to {0} ({1})'.format(sys.argv[1], proc.returncode))
+        exit(proc.returncode)
+
+if __name__ == '__main__':
+    main()
