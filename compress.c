4f1080e591ebdbdd51e7da1228fe7ec3dc5cc1de
Nathan Moinvaziri <nathan@nathanm.com>
2020-03-06 15:51:03 -0800

    Fixed compressBound calculation for quick deflate strategy. Worse case is 9 bits per literal plus the size of headers and footers for block and gzip format. Works out to the size of the input plus additional 13.67%.

--- compress.c
+++ compress.c
@@ -75,5 +75,11 @@ int ZEXPORT PREFIX(compress)(unsigned char *dest, z_size_t *destLen, const unsig
    this function needs to be updated.
  */
 z_size_t ZEXPORT PREFIX(compressBound)(z_size_t sourceLen) {
+#ifdef X86_QUICK_STRATEGY
+    /* Quick deflate strategy worse case is 9 bits per literal, rounded to nearest byte,
+       plus the size of block & gzip headers and footers */
+    return sourceLen + ((sourceLen + 13 + 7) >> 3) + 18;
+#else
     return sourceLen + (sourceLen >> 12) + (sourceLen >> 14) + (sourceLen >> 25) + 13;
+#endif
 }
