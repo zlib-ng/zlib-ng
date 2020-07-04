b924cf9f9cf0ff96f391e7f0b5f8bd3db6189542
Nathan Moinvaziri <nathan@nathanm.com>
2020-07-03 19:59:08 -0700

    Use 3 byte hash mask for slower compression levels > 6, 4 byte hash mask for faster compression levels.

--- insert_string_tpl.h
+++ insert_string_tpl.h
@@ -22,6 +22,8 @@
  *
  */
 
+#define HASH_TRIGGER_LEVEL 6
+
 /* ===========================================================================
  * Quick insert string str in the dictionary and set match_head to the previous head
  * of the hash chain (the most recent string with same hash key). Return
@@ -40,6 +42,8 @@ ZLIB_INTERNAL Pos QUICK_INSERT_STRING(deflate_state *const s, const Pos str) {
     val |= ((uint32_t)(strstart[2]) << 16);
     val |= ((uint32_t)(strstart[3]) << 24);
 #endif
+    if (s->level > HASH_TRIGGER_LEVEL)
+        val &= 0x00ffffff;
 
     UPDATE_HASH(s, h, val);
     hm = h & s->hash_mask;
@@ -63,6 +67,7 @@ ZLIB_INTERNAL Pos QUICK_INSERT_STRING(deflate_state *const s, const Pos str) {
 ZLIB_INTERNAL Pos INSERT_STRING(deflate_state *const s, const Pos str, unsigned int count) {
     Pos idx, ret;
     uint8_t *strstart, *strend;
+    uint32_t hash_mask;
 
     if (UNLIKELY(count == 0)) {
         return s->prev[str & s->w_mask];
@@ -70,6 +75,8 @@ ZLIB_INTERNAL Pos INSERT_STRING(deflate_state *const s, const Pos str, unsigned 
 
     strstart = s->window + str;
     strend = strstart + count - 1; /* last position */
+
+    hash_mask = (s->level > HASH_TRIGGER_LEVEL) ? 0x00ffffff : 0xffffffff;
 
     for (ret = 0, idx = str; strstart <= strend; idx++, strstart++) {
         uint32_t val, hm, h = 0;
@@ -82,7 +89,7 @@ ZLIB_INTERNAL Pos INSERT_STRING(deflate_state *const s, const Pos str, unsigned 
         val |= ((uint32_t)(strstart[2]) << 16);
         val |= ((uint32_t)(strstart[3]) << 24);
 #endif
-
+        val &= hash_mask;
         UPDATE_HASH(s, h, val);
         hm = h & s->hash_mask;
 
