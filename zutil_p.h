8543ec08520b8d19dc6eaed071063e408aaba210
Hans Kristian Rosbach <hk-git@circlestorm.org>
2020-09-12 22:55:02 +0200

    Increase zng_alloc alignment to 64-bytes for non-MacOS platforms.

--- zutil_p.h
+++ zutil_p.h
@@ -11,14 +11,14 @@
 #  include <malloc.h>
 #endif
 
-/* Function to allocate 16-byte aligned memory */
+/* Function to allocate 16 or 64-byte aligned memory */
 static inline void *zng_alloc(size_t size) {
 #if defined(_WIN32)
-    return (void *)_aligned_malloc(size, 16);
+    return (void *)_aligned_malloc(size, 64);
 #elif defined(__APPLE__)
     return (void *)malloc(size);     /* MacOS always aligns to 16 bytes */
 #else
-    return (void *)memalign(16, size);
+    return (void *)memalign(64, size);
 #endif
 }
 
