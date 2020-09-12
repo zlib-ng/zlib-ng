87628264d73df371f0d947de80d9295a05aadd43
Hans Kristian Rosbach <hk-git@circlestorm.org>
2020-09-12 19:54:38 +0200

    Simplify zng_calloc and zng_cfree. Make new static functions zng_alloc and zng_free available to other parts of the code. Always request aligned allocations, even if UNALIGNED_OK is set.

---
+++ zutil_p.h
@@ -0,0 +1,34 @@
+/* zutil_p.h -- Private inline functions used internally in zlib-ng
+ *
+ */
+
+#ifndef ZUTIL_P_H
+#define ZUTIL_P_H
+
+#ifdef __APPLE__
+#  include <stdlib.h>
+#else
+#  include <malloc.h>
+#endif
+
+/* Function to allocate 16-byte aligned memory */
+static inline void *zng_alloc(size_t size) {
+#if defined(_WIN32)
+    return (void *)_aligned_malloc(size, 16);
+#elif defined(__APPLE__)
+    return (void *)malloc(size);     /* MacOS always aligns to 16 bytes */
+#else
+    return (void *)memalign(16, size);
+#endif
+}
+
+/* Function that can free aligned memory */
+static inline void zng_free(void *ptr) {
+#if defined(_WIN32)
+    _aligned_free(ptr);
+#else
+    free(ptr);
+#endif
+}
+
+#endif
