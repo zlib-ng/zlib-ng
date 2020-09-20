91e5e39a03a4497ae2c89a71e695fe3c84c98567
Nathan Moinvaziri <nathan@nathanm.com>
2020-09-20 16:31:59 -0700

    Fixed tab indentation for inffixed_tbl.h.

--- tools/makefixed.c
+++ tools/makefixed.c
@@ -46,40 +46,40 @@ void makefixed(void) {
 
     memset(&state, 0, sizeof(state));
     buildfixedtables(&state);
-    puts("    /* inffixed_tbl.h -- table for decoding fixed codes");
-    puts("     * Generated automatically by makefixed().");
-    puts("     */");
+    puts("/* inffixed_tbl.h -- table for decoding fixed codes");
+    puts(" * Generated automatically by makefixed().");
+    puts(" */");
     puts("");
-    puts("    /* WARNING: this file should *not* be used by applications.");
-    puts("       It is part of the implementation of this library and is");
-    puts("       subject to change. Applications should only use zlib.h.");
-    puts("     */");
+    puts("/* WARNING: this file should *not* be used by applications.");
+    puts(" * It is part of the implementation of this library and is");
+    puts(" * subject to change. Applications should only use zlib.h.");
+    puts(" */");
     puts("");
     size = 1U << 9;
-    printf("    static const code lenfix[%u] = {", size);
+    printf("static const code lenfix[%u] = {", size);
     low = 0;
     for (;;) {
         if ((low % 7) == 0)
-            printf("\n        ");
+            printf("\n    ");
         printf("{%u,%u,%d}", (low & 127) == 99 ? 64 : state.lencode[low].op,
             state.lencode[low].bits, state.lencode[low].val);
         if (++low == size)
             break;
         putchar(',');
     }
-    puts("\n    };");
+    puts("\n};");
     size = 1U << 5;
-    printf("\n    static const code distfix[%u] = {", size);
+    printf("\nstatic const code distfix[%u] = {", size);
     low = 0;
     for (;;) {
         if ((low % 6) == 0)
-            printf("\n        ");
+            printf("\n    ");
         printf("{%u,%u,%d}", state.distcode[low].op, state.distcode[low].bits, state.distcode[low].val);
         if (++low == size)
             break;
         putchar(',');
     }
-    puts("\n    };");
+    puts("\n};");
 }
 
 // The output of this application can be piped out to recreate inffixed_tbl.h
