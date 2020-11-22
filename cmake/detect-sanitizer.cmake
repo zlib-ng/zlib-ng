ee295855b96c7aba0cb2e34ecc9f9fa8333ccada
Nathan Moinvaziri <nathan@nathanm.com>
2020-11-22 14:28:12 -0800

    Separate sanitizers so they can be run independently.

--- cmake/detect-sanitizer.cmake
+++ cmake/detect-sanitizer.cmake
@@ -29,33 +29,61 @@ macro(check_sanitizer_support known_checks supported_checks)
     set(${supported_checks} ${available_checks})
 endmacro()
 
-macro(add_memory_sanitizer_check)
+macro(add_address_sanitizer)
+    set(known_checks
+        address
+        pointer-compare
+        pointer-subtract
+        )
+
+    check_sanitizer_support("${known_checks}" supported_checks)
+    if(NOT ${supported_checks} STREQUAL "")
+        message(STATUS "Address sanitizer is enabled: ${supported_checks}")
+        set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fsanitize=${supported_checks}")
+    else()
+        message(STATUS "Address sanitizer is not supported")
+    endif()
+
+    if(CMAKE_CROSSCOMPILING_EMULATOR)
+        # Only check for leak sanitizer if not cross-compiling due to qemu crash
+        message(WARNING "Leak sanitizer is not supported when cross compiling")
+    else()
+        # Leak sanitizer requires address sanitizer
+        check_sanitizer_support("leak" supported_checks)
+        if(NOT ${supported_checks} STREQUAL "")
+            message(STATUS "Leak sanitizer is enabled: ${supported_checks}")
+            set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fsanitize=${supported_checks}")
+        else()
+            message(STATUS "Leak sanitizer is not supported")
+        endif()
+    endif()
+endmacro()
+
+macro(add_memory_sanitizer)
     check_sanitizer_support("memory" supported_checks)
     if(NOT ${supported_checks} STREQUAL "")
-        message(STATUS "Memory sanitizer is enabled")
+        message(STATUS "Memory sanitizer is enabled: ${supported_checks}")
         set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fsanitize=${supported_checks}")
     else()
         message(STATUS "Memory sanitizer is not supported")
     endif()
 endmacro()
 
-macro(add_sanitizer_checks)
+macro(add_undefined_sanitizer)
     set(known_checks
-        address
         array-bounds
         bool
         bounds
         builtin
         enum
+        float-cast-overflow
         float-divide-by-zero
         function
         integer-divide-by-zero
+        local-bounds
         null
         nonnull-attribute
-        object-size
-        pointer-compare             # Depends on 'address'
         pointer-overflow
-        pointer-subtract            # Depends on 'address'
         return
         returns-nonnull-attribute
         shift
@@ -64,23 +92,24 @@ macro(add_sanitizer_checks)
         signed-integer-overflow
         undefined
         unsigned-integer-overflow
+        unsigned-shift-base
         vla-bound
         vptr
         )
 
-    # Only check for leak sanitizer if not cross-compiling due to qemu crash
-    if(NOT CMAKE_CROSSCOMPILING_EMULATOR)
-        list(APPEND known_checks leak)
-    endif()
     # Only check for alignment sanitizer flag if unaligned access is not supported
     if(NOT UNALIGNED_OK)
         list(APPEND known_checks alignment)
+    endif()
+    # Object size sanitizer has no effect at -O0 and produces compiler warning if enabled
+    if(NOT CMAKE_C_FLAGS MATCHES "-O0")
+        list(APPEND known_checks object-size)
     endif()
 
     check_sanitizer_support("${known_checks}" supported_checks)
 
     if(NOT ${supported_checks} STREQUAL "")
-        message(STATUS "Supported sanitizers checks are enabled: ${supported_checks}")
+        message(STATUS "Undefined behavior sanitizer is enabled: ${supported_checks}")
         set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fsanitize=${supported_checks}")
 
         # Group sanitizer flag -fsanitize=undefined will automatically add alignment, even if
@@ -89,6 +118,6 @@ macro(add_sanitizer_checks)
             set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fno-sanitize=alignment")
         endif()
     else()
-        message(STATUS "Sanitizer checks are not supported")
+        message(STATUS "UNdefined behavior sanitizer is not supported")
     endif()
 endmacro()
