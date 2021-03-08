974f38ac2f97eeb88de773c4a615d8a32917823c
Hans Kristian Rosbach <hk-git@circlestorm.org>
2021-03-08 14:36:15 +0100

    Stable release 2.0.0

--- zutil.c
+++ zutil.c
@@ -21,7 +21,7 @@ z_const char * const PREFIX(z_errmsg)[10] = {
 };
 
 const char zlibng_string[] =
-    " zlib-ng 2.0.0-RC2 forked from zlib";
+    " zlib-ng 2.0.0 forked from zlib";
 
 #ifdef ZLIB_COMPAT
 const char * Z_EXPORT zlibVersion(void) {
