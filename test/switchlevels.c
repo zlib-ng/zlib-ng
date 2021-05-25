414ec0f00d85394849d0ce26c799faf094e2b6fb
Mika Lindqvist <postmaster@raasu.org>
2021-05-25 17:46:46 +0300

    Fix build under Cygwin.

--- test/switchlevels.c
+++ test/switchlevels.c
@@ -2,6 +2,8 @@
  * Each chunk is compressed with a user-specified level.
  */
 
+#define _POSIX_SOURCE 1  /* This file needs POSIX for fileno(). */
+
 #include "zbuild.h"
 #ifdef ZLIB_COMPAT
 #  include "zlib.h"
