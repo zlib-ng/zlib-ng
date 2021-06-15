6375d57257a22ce0fb8226c9ddb9af9ba89371ee
Nathan Moinvaziri <nathan@nathanm.com>
2021-06-14 19:55:09 -0700

    Separate slide_hash_c in the same way that insert_string_c is separated from deflate.c.

--- deflate.c
+++ deflate.c
@@ -185,50 +185,6 @@ static const config configuration_table[10] = {
     memset((unsigned char *)s->head, 0, HASH_SIZE * sizeof(*s->head)); \
   } while (0)
 
-/* ===========================================================================
- * Slide the hash table when sliding the window down (could be avoided with 32
- * bit values at the expense of memory usage). We slide even when level == 0 to
- * keep the hash table consistent if we switch back to level > 0 later.
- */
-static inline void slide_hash_c_chain(Pos *table, uint32_t entries, uint16_t wsize) {
-#ifdef NOT_TWEAK_COMPILER
-    table += entries;
-    do {
-        unsigned m;
-        m = *--table;
-        *table = (Pos)(m >= wsize ? m-wsize : 0);
-        /* If entries is not on any hash chain, prev[entries] is garbage but
-         * its value will never be used.
-         */
-    } while (--entries);
-#else
-    {
-    /* As of I make this change, gcc (4.8.*) isn't able to vectorize
-     * this hot loop using saturated-subtraction on x86-64 architecture.
-     * To avoid this defect, we can change the loop such that
-     *    o. the pointer advance forward, and
-     *    o. demote the variable 'm' to be local to the loop, and
-     *       choose type "Pos" (instead of 'unsigned int') for the
-     *       variable to avoid unnecessary zero-extension.
-     */
-        unsigned int i;
-        Pos *q = table;
-        for (i = 0; i < entries; i++) {
-            Pos m = *q;
-            Pos t = (Pos)wsize;
-            *q++ = (Pos)(m >= t ? m-t: 0);
-        }
-    }
-#endif /* NOT_TWEAK_COMPILER */
-}
-
-Z_INTERNAL void slide_hash_c(deflate_state *s) {
-    unsigned int wsize = s->w_size;
-
-    slide_hash_c_chain(s->head, HASH_SIZE, wsize);
-    slide_hash_c_chain(s->prev, wsize, wsize);
-        }
-
 /* ========================================================================= */
 int32_t Z_EXPORT PREFIX(deflateInit_)(PREFIX3(stream) *strm, int32_t level, const char *version, int32_t stream_size) {
     return PREFIX(deflateInit2_)(strm, level, Z_DEFLATED, MAX_WBITS, DEF_MEM_LEVEL, Z_DEFAULT_STRATEGY, version, stream_size);
