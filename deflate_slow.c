a0fe6c590d04ce67a52b417287651fa64d8d1e53
Nathan Moinvaziri <nathan@nathanm.com>
2021-06-21 20:39:47 -0700

    Use longest_match_slow in deflate_slow.

--- deflate_slow.c
+++ deflate_slow.c
@@ -55,7 +55,7 @@ Z_INTERNAL block_state deflate_slow(deflate_state *s, int flush) {
              * of window index 0 (in particular we have to avoid a match
              * of the string with itself at the start of the input file).
              */
-            match_len = functable.longest_match(s, hash_head);
+            match_len = functable.longest_match_slow(s, hash_head);
             /* longest_match() sets match_start */
 
             if (match_len <= 5 && (s->strategy == Z_FILTERED)) {
