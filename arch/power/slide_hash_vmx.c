f1544cf1a06eac3819f564043b79e00b0c37d8b0
Mika Lindqvist <postmaster@raasu.org>
2021-06-22 22:19:13 +0300

    [PowerPC] Use templatized code for slide_hash as code for VMX and VSX is very similar * Any differences can be handled using compiler options or added as macros before including template header

--- arch/power/slide_hash_vmx.c
+++ arch/power/slide_hash_vmx.c
@@ -4,31 +4,7 @@
  */
 #ifdef PPC_VMX_SLIDEHASH
 
-#include <altivec.h>
-#include "zbuild.h"
-#include "deflate.h"
-
-static inline void slide_hash_chain(Pos *table, uint32_t entries, uint16_t wsize) {
-    const vector unsigned short vmx_wsize = vec_splats(wsize);
-    Pos *p = table;
-
-    do {
-        vector unsigned short value, result;
-
-        value = vec_ld(0, p);
-        result = vec_subs(value, vmx_wsize);
-        vec_st(result, 0, p);
-
-        p += 8;
-        entries -= 8;
-   } while (entries > 0);
-}
-
-void Z_INTERNAL slide_hash_vmx(deflate_state *s) {
-    uint16_t wsize = s->w_size;
-
-    slide_hash_chain(s->head, HASH_SIZE, wsize);
-    slide_hash_chain(s->prev, wsize, wsize);
-}
+#define SLIDE_PPC slide_hash_vmx
+#include "slide_ppc_tpl.h"
 
 #endif /* PPC_VMX_SLIDEHASH */
