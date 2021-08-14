a4f1da5e2837a3a64fc187670d33cf944bd5e3b8
Nathan Moinvaziri <nathan@solidstatenetworks.com>
2021-08-14 16:40:23 -0700

    Use ZFREE_STATE macro in inflateBackEnd instead of ZFREE.

--- infback.c
+++ infback.c
@@ -490,7 +490,7 @@ int32_t Z_EXPORT PREFIX(inflateBack)(PREFIX3(stream) *strm, in_func in, void *in
 int32_t Z_EXPORT PREFIX(inflateBackEnd)(PREFIX3(stream) *strm) {
     if (strm == NULL || strm->state == NULL || strm->zfree == NULL)
         return Z_STREAM_ERROR;
-    ZFREE(strm, strm->state);
+    ZFREE_STATE(strm, strm->state);
     strm->state = NULL;
     Tracev((stderr, "inflate: end\n"));
     return Z_OK;
