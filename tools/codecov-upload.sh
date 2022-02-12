0e493c86969099ef246a2e86e58f14dd1ccf7b5c
Nathan Moinvaziri <nathan@nathanm.com>
2022-02-11 17:23:31 -0800

    Switch to using Codecov GitHub Action.

--- tools/codecov-upload.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -ux
-cd "$CODECOV_DIR"
-python -m codecov --required --flags "$CODECOV_FLAGS" --name "$CODECOV_NAME" --gcov-exec="$CODECOV_EXEC"
-if [ $? -ne 0 ]; then
-  sleep 30
-  python -m codecov --required --flags "$CODECOV_FLAGS" --name "$CODECOV_NAME" --gcov-exec="$CODECOV_EXEC" --tries=25
-fi
-exit $?
