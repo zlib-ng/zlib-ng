0a59b4e7454b39c20a4eca88716e18d93431f96f
Piotr Kubaj <pkubaj@FreeBSD.org>
2022-09-28 00:57:46 +0200

    Add FreeBSD/powerpc* support to cmake/detect-intrinsics.cmake

--- cmake/detect-intrinsics.cmake
+++ cmake/detect-intrinsics.cmake
@@ -297,8 +297,17 @@ macro(check_ppc_intrinsics)
     set(CMAKE_REQUIRED_FLAGS "${PPCFLAGS} ${NATIVEFLAG}")
     check_c_source_compiles(
         "#include <sys/auxv.h>
+        #ifdef __FreeBSD__
+        #include <machine/cpu.h>
+        #endif
         int main() {
+        #ifdef __FreeBSD__
+            unsigned long hwcap;
+            elf_aux_info(AT_HWCAP, &hwcap, sizeof(hwcap));
+            return (hwcap & PPC_FEATURE_HAS_ALTIVEC);
+        #else
             return (getauxval(AT_HWCAP) & PPC_FEATURE_HAS_ALTIVEC);
+        #endif
         }"
         HAVE_VMX
     )
@@ -315,8 +324,17 @@ macro(check_power8_intrinsics)
     set(CMAKE_REQUIRED_FLAGS "${POWER8FLAG} ${NATIVEFLAG}")
     check_c_source_compiles(
         "#include <sys/auxv.h>
+        #ifdef __FreeBSD__
+        #include <machine/cpu.h>
+        #endif
         int main() {
+        #ifdef __FreeBSD__
+            unsigned long hwcap;
+            elf_aux_info(AT_HWCAP2, &hwcap, sizeof(hwcap));
+            return (hwcap & PPC_FEATURE2_ARCH_2_07);
+        #else
             return (getauxval(AT_HWCAP2) & PPC_FEATURE2_ARCH_2_07);
+        #endif
         }"
         HAVE_POWER8_INTRIN
     )
