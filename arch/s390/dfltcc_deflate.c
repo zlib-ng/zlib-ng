cc06335176d4cd1dfceaf8b6bcb757ad73da73be
Ilya Leoshkevich <iii@linux.ibm.com>
2022-12-22 10:34:57 +0100

    IBM zSystems DFLTCC: Do not update strm.adler for raw streams
    Commit d38dd9240f2d ("IBM Z DFLTCC: Fix updating strm.adler with
    inflate()") broke libxml2, as can be seen with the repro from [1]:
    
        $ echo "<a></a>" | gzip >file.xml.gz
        $ python3 -c 'import libxml2; libxml2.parseFile("file.xml.gz")'
        file.xml.gz:1: parser error : Document is empty
    
    This is because libxml2 expects strm.adler to be untouched for raw
    streams.
    
    Fix this and a similar issue in deflate by adding state->wrap checks.
    Add tests.
    
    [1] https://bugzilla.redhat.com/show_bug.cgi?id=2155328
    [2] https://gitlab.gnome.org/GNOME/libxml2/-/blob/v2.10.3/xzlib.c#L607

--- arch/s390/dfltcc_deflate.c
+++ arch/s390/dfltcc_deflate.c
@@ -240,7 +240,10 @@ again:
         *strm->next_out = (unsigned char)state->bi_buf;
     /* Honor history and check value */
     param->nt = 0;
-    param->cv = state->wrap == 2 ? ZSWAP32(state->crc_fold.value) : strm->adler;
+    if (state->wrap == 1)
+        param->cv = strm->adler;
+    else if (state->wrap == 2)
+        param->cv = ZSWAP32(state->crc_fold.value);
 
     /* When opening a block, choose a Huffman-Table Type */
     if (!param->bcf) {
@@ -271,10 +274,10 @@ again:
         state->bi_buf = 0; /* Avoid accessing next_out */
     else
         state->bi_buf = *strm->next_out & ((1 << state->bi_valid) - 1);
-    if (state->wrap == 2)
-        state->crc_fold.value = ZSWAP32(param->cv);
-    else
+    if (state->wrap == 1)
         strm->adler = param->cv;
+    else if (state->wrap == 2)
+        state->crc_fold.value = ZSWAP32(param->cv);
 
     /* Unmask the input data */
     strm->avail_in += masked_avail_in;
