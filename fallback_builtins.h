17331431e085e535094e3fe29e1a5a473c466f23
Hans Kristian Rosbach <hk-git@circlestorm.org>
2023-02-04 21:54:09 +0100

    Replace __builtin_ctz[ll] fallback functions with branchless implementations. Added debug assert check for value = 0. Added more details to the comment to avoid future confusion. Added fallback logic for older MSVC versions, just in case.

--- fallback_builtins.h
+++ fallback_builtins.h
@@ -9,36 +9,37 @@
 #  include "arch/x86/x86_features.h"
 #endif
 
-/* This is not a general purpose replacement for __builtin_ctz. The function expects that value is != 0
- * Because of that assumption trailing_zero is not initialized and the return value of _BitScanForward is not checked
+/* This is not a general purpose replacement for __builtin_ctz. The function expects that value is != 0.
+ * Because of that assumption trailing_zero is not initialized and the return value is not checked.
+ * Tzcnt and bsf give identical results except when input value is 0, therefore this can not be allowed.
+ * If tzcnt instruction is not supported, the cpu will itself execute bsf instead.
+ * Performance tzcnt/bsf is identical on Intel cpu, tzcnt is faster than bsf on AMD cpu.
  */
 static __forceinline int __builtin_ctz(unsigned int value) {
-#ifdef X86_FEATURES
-#  ifndef X86_NOCHECK_TZCNT
-    if (x86_cpu_has_tzcnt)
-#  endif
-        return (int)_tzcnt_u32(value);
-#endif
+    Assert(value != 0, "Invalid input value: 0");
+# if defined(X86_FEATURES) && !(_MSC_VER < 1700)
+    return (int)_tzcnt_u32(value);
+# else
     unsigned long trailing_zero;
     _BitScanForward(&trailing_zero, value);
     return (int)trailing_zero;
+# endif
 }
 #define HAVE_BUILTIN_CTZ
 
 #ifdef _M_AMD64
-/* This is not a general purpose replacement for __builtin_ctzll. The function expects that value is != 0
- * Because of that assumption trailing_zero is not initialized and the return value of _BitScanForward64 is not checked
+/* This is not a general purpose replacement for __builtin_ctzll. The function expects that value is != 0.
+ * Because of that assumption trailing_zero is not initialized and the return value is not checked.
  */
 static __forceinline int __builtin_ctzll(unsigned long long value) {
-#ifdef X86_FEATURES
-#  ifndef X86_NOCHECK_TZCNT
-    if (x86_cpu_has_tzcnt)
-#  endif
-        return (int)_tzcnt_u64(value);
-#endif
+    Assert(value != 0, "Invalid input value: 0");
+# if defined(X86_FEATURES) && !(_MSC_VER < 1700)
+    return (int)_tzcnt_u64(value);
+# else
     unsigned long trailing_zero;
     _BitScanForward64(&trailing_zero, value);
     return (int)trailing_zero;
+# endif
 }
 #define HAVE_BUILTIN_CTZLL
 #endif // Microsoft AMD64
