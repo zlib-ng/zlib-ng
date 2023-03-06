b8c2114d51ca120f76e0cdad706c8f0c15d1f829
Ilya Leoshkevich <iii@linux.ibm.com>
2023-03-06 23:25:04 +0100

    IBM zSystems: Use HWCAP_S390_VXRS
    glibc defines HWCAP_S390_VX and, since v2.33, its alias
    HWCAP_S390_VXRS; musl has only HWCAP_S390_VXRS.
    
    Use the common HWCAP_S390_VXRS, define it as HWCAP_S390_VX if
    necessary.

--- arch/s390/s390_features.c
+++ arch/s390/s390_features.c
@@ -5,6 +5,10 @@
 #  include <sys/auxv.h>
 #endif
 
+#ifndef HWCAP_S390_VXRS
+#define HWCAP_S390_VXRS HWCAP_S390_VX
+#endif
+
 void Z_INTERNAL s390_check_features(struct s390_cpu_features *features) {
-    features->has_vx = getauxval(AT_HWCAP) & HWCAP_S390_VX;
+    features->has_vx = getauxval(AT_HWCAP) & HWCAP_S390_VXRS;
 }
