dd8dc56e6196c01ad3c80493ef93396479871c1e
Vladislav Shchapov <vladislav@shchapov.ru>
2023-12-16 14:10:11 +0500

    Fix buffer overflow in compare256_rle benchmark
    Signed-off-by: Vladislav Shchapov <vladislav@shchapov.ru>

--- test/benchmarks/benchmark_compare256_rle.cc
+++ test/benchmarks/benchmark_compare256_rle.cc
@@ -33,7 +33,7 @@ public:
     }
 
     void Bench(benchmark::State& state, compare256_rle_func compare256_rle) {
-        int32_t match_len = (int32_t)state.range(0);
+        int32_t match_len = (int32_t)state.range(0) - 1;
         uint32_t len;
 
         str2[match_len] = 0;
