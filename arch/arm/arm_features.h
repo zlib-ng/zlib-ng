8c8cca76388b74e3f52eae5db335215d24b0b6b8
Nathan Moinvaziri <nathan@nathanm.com>
2024-01-22 10:48:52 -0800

    Remove extern keyword from cpu_feature function declarations.

--- arch/arm/arm_features.h
+++ arch/arm/arm_features.h
@@ -16,38 +16,38 @@ void Z_INTERNAL arm_check_features(struct arm_cpu_features *features);
 #ifdef CPU_FEATURES_H_
 
 #ifdef ARM_NEON
-extern uint32_t adler32_neon(uint32_t adler, const uint8_t *buf, size_t len);
-extern uint32_t chunksize_neon(void);
-extern uint8_t* chunkmemset_safe_neon(uint8_t *out, unsigned dist, unsigned len, unsigned left);
+uint32_t adler32_neon(uint32_t adler, const uint8_t *buf, size_t len);
+uint32_t chunksize_neon(void);
+uint8_t* chunkmemset_safe_neon(uint8_t *out, unsigned dist, unsigned len, unsigned left);
 
 #  ifdef HAVE_BUILTIN_CTZLL
-    extern uint32_t compare256_neon(const uint8_t *src0, const uint8_t *src1);
+    uint32_t compare256_neon(const uint8_t *src0, const uint8_t *src1);
 #    ifdef DEFLATE_H_
-        extern uint32_t longest_match_neon(deflate_state *const s, Pos cur_match);
-        extern uint32_t longest_match_slow_neon(deflate_state *const s, Pos cur_match);
+        uint32_t longest_match_neon(deflate_state *const s, Pos cur_match);
+        uint32_t longest_match_slow_neon(deflate_state *const s, Pos cur_match);
 #    endif
 #  endif
 #  ifdef DEFLATE_H_
-    extern void slide_hash_neon(deflate_state *s);
+    void slide_hash_neon(deflate_state *s);
 #  endif
 #  ifdef INFLATE_H_
-    extern void inflate_fast_neon(PREFIX3(stream) *strm, uint32_t start);
+    void inflate_fast_neon(PREFIX3(stream) *strm, uint32_t start);
 #  endif
 #endif
 
 #ifdef ARM_ACLE
-extern uint32_t crc32_acle(uint32_t crc, const uint8_t *buf, size_t len);
+uint32_t crc32_acle(uint32_t crc, const uint8_t *buf, size_t len);
 
 #  ifdef DEFLATE_H_
-    extern void insert_string_acle(deflate_state *const s, const uint32_t str, uint32_t count);
-    extern Pos quick_insert_string_acle(deflate_state *const s, const uint32_t str);
-    extern uint32_t update_hash_acle(deflate_state *const s, uint32_t h, uint32_t val);
+    void insert_string_acle(deflate_state *const s, const uint32_t str, uint32_t count);
+    Pos quick_insert_string_acle(deflate_state *const s, const uint32_t str);
+    uint32_t update_hash_acle(deflate_state *const s, uint32_t h, uint32_t val);
 #  endif
 #endif
 
 #ifdef ARM_SIMD
 #  ifdef DEFLATE_H_
-    extern void slide_hash_armv6(deflate_state *s);
+    void slide_hash_armv6(deflate_state *s);
 #  endif
 #endif
 
--- arch/generic/generic_features.h
+++ arch/generic/generic_features.h
@@ -9,51 +9,51 @@ typedef uint32_t (*adler32_func)(uint32_t adler, const uint8_t *buf, size_t len)
 typedef uint32_t (*compare256_func)(const uint8_t *src0, const uint8_t *src1);
 typedef uint32_t (*crc32_func)(uint32_t crc32, const uint8_t *buf, size_t len);
 
-extern uint32_t adler32_c(uint32_t adler, const uint8_t *buf, size_t len);
-extern uint32_t chunksize_c(void);
-extern uint8_t* chunkmemset_safe_c(uint8_t *out, unsigned dist, unsigned len, unsigned left);
+uint32_t adler32_c(uint32_t adler, const uint8_t *buf, size_t len);
+uint32_t chunksize_c(void);
+uint8_t* chunkmemset_safe_c(uint8_t *out, unsigned dist, unsigned len, unsigned left);
 #ifdef INFLATE_H_
-extern void     inflate_fast_c(PREFIX3(stream) *strm, uint32_t start);
+void     inflate_fast_c(PREFIX3(stream) *strm, uint32_t start);
 #endif
 
-extern uint32_t PREFIX(crc32_braid)(uint32_t crc, const uint8_t *buf, size_t len);
+uint32_t PREFIX(crc32_braid)(uint32_t crc, const uint8_t *buf, size_t len);
 
-extern uint32_t compare256_c(const uint8_t *src0, const uint8_t *src1);
+uint32_t compare256_c(const uint8_t *src0, const uint8_t *src1);
 #if defined(UNALIGNED_OK) && BYTE_ORDER == LITTLE_ENDIAN
-extern uint32_t compare256_unaligned_16(const uint8_t *src0, const uint8_t *src1);
+uint32_t compare256_unaligned_16(const uint8_t *src0, const uint8_t *src1);
 #  ifdef HAVE_BUILTIN_CTZ
-    extern uint32_t compare256_unaligned_32(const uint8_t *src0, const uint8_t *src1);
+    uint32_t compare256_unaligned_32(const uint8_t *src0, const uint8_t *src1);
 #  endif
 #  if defined(UNALIGNED64_OK) && defined(HAVE_BUILTIN_CTZLL)
-    extern uint32_t compare256_unaligned_64(const uint8_t *src0, const uint8_t *src1);
+    uint32_t compare256_unaligned_64(const uint8_t *src0, const uint8_t *src1);
 #  endif
 #endif
 
 #ifdef DEFLATE_H_
 typedef void    (*slide_hash_func)(deflate_state *s);
 
-extern void     insert_string_c(deflate_state *const s, const uint32_t str, uint32_t count);
-extern Pos      quick_insert_string_c(deflate_state *const s, const uint32_t str);
-extern void     slide_hash_c(deflate_state *s);
-extern uint32_t update_hash_c(deflate_state *const s, uint32_t h, uint32_t val);
+void     insert_string_c(deflate_state *const s, const uint32_t str, uint32_t count);
+Pos      quick_insert_string_c(deflate_state *const s, const uint32_t str);
+void     slide_hash_c(deflate_state *s);
+uint32_t update_hash_c(deflate_state *const s, uint32_t h, uint32_t val);
 
-extern uint32_t longest_match_c(deflate_state *const s, Pos cur_match);
+uint32_t longest_match_c(deflate_state *const s, Pos cur_match);
 #  if defined(UNALIGNED_OK) && BYTE_ORDER == LITTLE_ENDIAN
-    extern uint32_t longest_match_unaligned_16(deflate_state *const s, Pos cur_match);
+    uint32_t longest_match_unaligned_16(deflate_state *const s, Pos cur_match);
 #    ifdef HAVE_BUILTIN_CTZ
-        extern uint32_t longest_match_unaligned_32(deflate_state *const s, Pos cur_match);
+        uint32_t longest_match_unaligned_32(deflate_state *const s, Pos cur_match);
 #    endif
 #    if defined(UNALIGNED64_OK) && defined(HAVE_BUILTIN_CTZLL)
-        extern uint32_t longest_match_unaligned_64(deflate_state *const s, Pos cur_match);
+        uint32_t longest_match_unaligned_64(deflate_state *const s, Pos cur_match);
 #    endif
 #  endif
 
-extern uint32_t longest_match_slow_c(deflate_state *const s, Pos cur_match);
+uint32_t longest_match_slow_c(deflate_state *const s, Pos cur_match);
 #  if defined(UNALIGNED_OK) && BYTE_ORDER == LITTLE_ENDIAN
-    extern uint32_t longest_match_slow_unaligned_16(deflate_state *const s, Pos cur_match);
-    extern uint32_t longest_match_slow_unaligned_32(deflate_state *const s, Pos cur_match);
+    uint32_t longest_match_slow_unaligned_16(deflate_state *const s, Pos cur_match);
+    uint32_t longest_match_slow_unaligned_32(deflate_state *const s, Pos cur_match);
 #    ifdef UNALIGNED64_OK
-        extern uint32_t longest_match_slow_unaligned_64(deflate_state *const s, Pos cur_match);
+        uint32_t longest_match_slow_unaligned_64(deflate_state *const s, Pos cur_match);
 #    endif
 #  endif
 
