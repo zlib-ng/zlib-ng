c1a3f17408675c6ee932e9a4ed7d04a0ae42a545
Mika Lindqvist <postmaster@raasu.org>
2024-01-24 22:48:00 +0200

    Fix regression caused by 2fa631e029084b75acd81db5d33fd4aa802fd082 * POWER8/9 feature checks were enabled even if the toolchain didn't support AT_HWCAP2 * Add detection if we need to include <linux/auxvec.h>

--- arch/power/power_features.c
+++ arch/power/power_features.c
@@ -1,11 +1,14 @@
 /* power_features.c - POWER feature check
  * Copyright (C) 2020 Matheus Castanho <msc@linux.ibm.com>, IBM
- * Copyright (C) 2021-2022 Mika T. Lindqvist <postmaster@raasu.org>
+ * Copyright (C) 2021-2024 Mika T. Lindqvist <postmaster@raasu.org>
  * For conditions of distribution and use, see copyright notice in zlib.h
  */
 
 #ifdef HAVE_SYS_AUXV_H
 #  include <sys/auxv.h>
+#endif
+#ifdef POWER_NEED_AUXVEC_H
+#  include <linux/auxvec.h>
 #endif
 #ifdef __FreeBSD__
 #  include <machine/cpu.h>
