3cf0bc39ab830edba68588f2aed04382f5fe1908
Nathan Moinvaziri <nathan@nathanm.com>
2024-01-25 06:09:25 -0800

    Split cpu_features.h by architecture.

--- arch/power/power_features.h
+++ arch/power/power_features.h
@@ -15,4 +15,35 @@ struct power_cpu_features {
 
 void Z_INTERNAL power_check_features(struct power_cpu_features *features);
 
+#ifdef CPU_FEATURES_H_
+
+#ifdef PPC_VMX
+extern uint32_t adler32_vmx(uint32_t adler, const uint8_t *buf, size_t len);
+#  ifdef DEFLATE_H_
+    extern void slide_hash_vmx(deflate_state *s);
+#  endif
+#endif
+
+#ifdef POWER8_VSX
+extern uint32_t adler32_power8(uint32_t adler, const uint8_t *buf, size_t len);
+extern uint32_t chunksize_power8(void);
+extern uint8_t* chunkmemset_safe_power8(uint8_t *out, unsigned dist, unsigned len, unsigned left);
+extern uint32_t crc32_power8(uint32_t crc, const uint8_t *buf, size_t len);
+extern void     inflate_fast_power8(PREFIX3(stream) *strm, uint32_t start);
+
+#  ifdef DEFLATE_H_
+    extern void slide_hash_power8(deflate_state *s);
+#  endif
+#endif
+
+#ifdef POWER9
+extern uint32_t compare256_power9(const uint8_t *src0, const uint8_t *src1);
+#  ifdef DEFLATE_H_
+    extern uint32_t longest_match_power9(deflate_state *const s, Pos cur_match);
+    extern uint32_t longest_match_slow_power9(deflate_state *const s, Pos cur_match);
+#  endif
+#endif
+
+#endif
+
 #endif /* POWER_H_ */
--- arch/x86/x86_features.h
+++ arch/x86/x86_features.h
@@ -1,7 +1,7 @@
 /* x86_features.h -- check for CPU features
-* Copyright (C) 2013 Intel Corporation Jim Kukunas
-* For conditions of distribution and use, see copyright notice in zlib.h
-*/
+ * Copyright (C) 2013 Intel Corporation Jim Kukunas
+ * For conditions of distribution and use, see copyright notice in zlib.h
+ */
 
 #ifndef X86_FEATURES_H_
 #define X86_FEATURES_H_
@@ -20,5 +20,82 @@ struct x86_cpu_features {
 };
 
 void Z_INTERNAL x86_check_features(struct x86_cpu_features *features);
+
+#ifdef CPU_FEATURES_H_
+
+#include "fallback_builtins.h"
+#include "crc32.h"
+
+#ifdef X86_SSE2
+extern uint32_t chunksize_sse2(void);
+extern uint8_t* chunkmemset_safe_sse2(uint8_t *out, unsigned dist, unsigned len, unsigned left);
+extern void     inflate_fast_sse2(PREFIX3(stream) *strm, uint32_t start);
+
+#  ifdef HAVE_BUILTIN_CTZ
+    extern uint32_t compare256_sse2(const uint8_t *src0, const uint8_t *src1);
+#    ifdef DEFLATE_H_
+        extern uint32_t longest_match_sse2(deflate_state *const s, Pos cur_match);
+        extern uint32_t longest_match_slow_sse2(deflate_state *const s, Pos cur_match);
+        extern void slide_hash_sse2(deflate_state *s);
+#    endif
+#  endif
+#endif
+
+#ifdef X86_SSSE3
+extern uint32_t adler32_ssse3(uint32_t adler, const uint8_t *buf, size_t len);
+extern uint8_t* chunkmemset_safe_ssse3(uint8_t *out, unsigned dist, unsigned len, unsigned left);
+extern void     inflate_fast_ssse3(PREFIX3(stream) *strm, uint32_t start);
+#endif
+
+#ifdef X86_SSE42
+extern uint32_t adler32_fold_copy_sse42(uint32_t adler, uint8_t *dst, const uint8_t *src, size_t len);
+#  ifdef DEFLATE_H_
+    extern void insert_string_sse42(deflate_state *const s, const uint32_t str, uint32_t count);
+    extern Pos quick_insert_string_sse42(deflate_state *const s, const uint32_t str);
+    extern uint32_t update_hash_sse42(deflate_state *const s, uint32_t h, uint32_t val);
+#  endif
+#endif
+
+#ifdef X86_AVX2
+extern uint32_t adler32_avx2(uint32_t adler, const uint8_t *buf, size_t len);
+extern uint32_t adler32_fold_copy_avx2(uint32_t adler, uint8_t *dst, const uint8_t *src, size_t len);
+extern uint32_t chunksize_avx2(void);
+extern uint8_t* chunkmemset_safe_avx2(uint8_t *out, unsigned dist, unsigned len, unsigned left);
+extern void     inflate_fast_avx2(PREFIX3(stream) *strm, uint32_t start);
+
+#  ifdef HAVE_BUILTIN_CTZ
+    extern uint32_t compare256_avx2(const uint8_t *src0, const uint8_t *src1);
+#    ifdef DEFLATE_H_
+        extern uint32_t longest_match_avx2(deflate_state *const s, Pos cur_match);
+        extern uint32_t longest_match_slow_avx2(deflate_state *const s, Pos cur_match);
+        extern void slide_hash_avx2(deflate_state *s);
+#    endif
+#  endif
+#endif
+#ifdef X86_AVX512
+extern uint32_t adler32_avx512(uint32_t adler, const uint8_t *buf, size_t len);
+extern uint32_t adler32_fold_copy_avx512(uint32_t adler, uint8_t *dst, const uint8_t *src, size_t len);
+#endif
+#ifdef X86_AVX512VNNI
+extern uint32_t adler32_avx512_vnni(uint32_t adler, const uint8_t *buf, size_t len);
+extern uint32_t adler32_fold_copy_avx512_vnni(uint32_t adler, uint8_t *dst, const uint8_t *src, size_t len);
+#endif
+
+#ifdef X86_PCLMULQDQ_CRC
+extern uint32_t crc32_fold_pclmulqdq_reset(crc32_fold *crc);
+extern void     crc32_fold_pclmulqdq_copy(crc32_fold *crc, uint8_t *dst, const uint8_t *src, size_t len);
+extern void     crc32_fold_pclmulqdq(crc32_fold *crc, const uint8_t *src, size_t len, uint32_t init_crc);
+extern uint32_t crc32_fold_pclmulqdq_final(crc32_fold *crc);
+extern uint32_t crc32_pclmulqdq(uint32_t crc32, const uint8_t *buf, size_t len);
+#endif
+#ifdef X86_VPCLMULQDQ_CRC
+extern uint32_t crc32_fold_vpclmulqdq_reset(crc32_fold *crc);
+extern void     crc32_fold_vpclmulqdq_copy(crc32_fold *crc, uint8_t *dst, const uint8_t *src, size_t len);
+extern void     crc32_fold_vpclmulqdq(crc32_fold *crc, const uint8_t *src, size_t len, uint32_t init_crc);
+extern uint32_t crc32_fold_vpclmulqdq_final(crc32_fold *crc);
+extern uint32_t crc32_vpclmulqdq(uint32_t crc32, const uint8_t *buf, size_t len);
+#endif
+
+#endif
 
 #endif /* CPU_H_ */
