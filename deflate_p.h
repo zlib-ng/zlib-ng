68506e1b292f82e8e7e6ff6193cf1bee003cbbbb
Hans Kristian Rosbach <hk-git@circlestorm.org>
2024-11-02 16:36:24 +0100

    Remove Assert that is always true due to limited range of data-type

--- deflate_p.h
+++ deflate_p.h
@@ -72,7 +72,6 @@ static inline int zng_tr_tally_lit(deflate_state *s, unsigned char c) {
 #endif
     s->dyn_ltree[c].Freq++;
     Tracevv((stderr, "%c", c));
-    Assert(c <= (STD_MAX_MATCH-STD_MIN_MATCH), "zng_tr_tally: bad literal");
     return (s->sym_next == s->sym_end);
 }
 
