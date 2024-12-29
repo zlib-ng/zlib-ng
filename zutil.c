9ed235ac7ab86fa5a3ba60186567e23959cc0202
Hans Kristian Rosbach <hk-git@circlestorm.org>
2024-12-29 19:01:35 +0100

    2.2.3 Release

--- zutil.c
+++ zutil.c
@@ -21,7 +21,7 @@ z_const char * const PREFIX(z_errmsg)[10] = {
 };
 
 const char PREFIX3(vstring)[] =
-    " zlib-ng 2.2.2";
+    " zlib-ng 2.2.3";
 
 #ifdef ZLIB_COMPAT
 const char * Z_EXPORT zlibVersion(void) {
