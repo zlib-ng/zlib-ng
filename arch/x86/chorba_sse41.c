f34b18720310049189d16bfdc81e710f06d5d17e
Pavel P <pavlov.pavel@gmail.com>
2025-04-26 21:02:32 +0200

    Match function declaration for chorba_small_nondestructive_sse2

--- arch/x86/chorba_sse41.c
+++ arch/x86/chorba_sse41.c
@@ -11,7 +11,7 @@
 #include <assert.h>
 
 extern uint32_t crc32_braid_base(uint32_t c, const uint8_t *buf, size_t len);
-extern uint32_t chorba_small_nondestructive_sse2(uint32_t c, uint64_t *aligned_buf, size_t aligned_len);
+extern uint32_t chorba_small_nondestructive_sse2(uint32_t c, const uint64_t *aligned_buf, size_t aligned_len);
 
 #define READ_NEXT(in, off, a, b) do { \
         a = _mm_load_si128((__m128i*)(in + off / sizeof(uint64_t))); \
