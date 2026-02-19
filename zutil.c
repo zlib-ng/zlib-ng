23e27467df17ef0c4ffebcb9a5aa0d3abe24af5f
Vladislav Shchapov <vladislav@shchapov.ru>
2026-02-19 22:02:13 +0500

    Set version for the develop branch to 2.3.90
    Signed-off-by: Vladislav Shchapov <vladislav@shchapov.ru>

--- zutil.c
+++ zutil.c
@@ -21,7 +21,7 @@ z_const char * const PREFIX(z_errmsg)[10] = {
 };
 
 const char PREFIX3(vstring)[] =
-    " zlib-ng 2.3.1";
+    " zlib-ng 2.3.90 devel";
 
 #ifdef ZLIB_COMPAT
 const char * Z_EXPORT zlibVersion(void) {
