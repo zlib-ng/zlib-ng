b1b36f2c35a655128868df9154ef0cb2d8cec2ad
Hans Kristian Rosbach <hk-git@circlestorm.org>
2026-04-15 14:27:27 +0200

    Fix UBSAN implicit conversion warning in test/test_shared_ng.h.

--- test/test_shared_ng.h
+++ test/test_shared_ng.h
@@ -9,12 +9,12 @@ static inline int deflate_prime_32(PREFIX3(stream) *stream, uint32_t value) {
     int err;
 
 #ifndef TEST_STOCK_ZLIB
-    err = PREFIX(deflatePrime)(stream, 32, value);
+    err = PREFIX(deflatePrime)(stream, 32, (int32_t)value);
 #else
     /* zlib's deflatePrime() takes at most 16 bits */
-    err = PREFIX(deflatePrime)(stream, 16, value & 0xffff);
+    err = PREFIX(deflatePrime)(stream, 16, (int32_t)(value & 0xffff));
     if (err != Z_OK) return err;
-    err = PREFIX(deflatePrime)(stream, 16, value >> 16);
+    err = PREFIX(deflatePrime)(stream, 16, (int32_t)(value >> 16));
 #endif
 
     return err;
