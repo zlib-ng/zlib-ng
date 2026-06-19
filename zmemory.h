128b0a919ee4b23c6c3601ce438bb6dc3880a2d6
Mika Lindqvist <postmaster@raasu.org>
2026-06-19 19:58:00 +0300

    Add Z_RESTRICT to zng_memcmp functions * Use zng_memread and zng_memwrite functions with Visual C++ too to allow   unaligned memory access

--- zmemory.h
+++ zmemory.h
@@ -72,24 +72,24 @@ static inline void zng_memwrite_8(void *ptr, uint64_t val) {
 /* Use zng_memread_* instead of memcmp to avoid older compilers not converting memcmp
    calls to unaligned comparisons when unaligned access is supported. Use memcmp only when
    unaligned support is not available to avoid an extra call to memcpy. */
-static inline int32_t zng_memcmp_2(const void *src0, const void *src1) {
-#if defined(HAVE_MAY_ALIAS)
+static inline int32_t zng_memcmp_2(const void * Z_RESTRICT src0, const void * Z_RESTRICT src1) {
+#if defined(HAVE_MAY_ALIAS) || defined(_MSC_VER)
     return zng_memread_2(src0) != zng_memread_2(src1);
 #else
     return memcmp(src0, src1, 2);
 #endif
 }
 
-static inline int32_t zng_memcmp_4(const void *src0, const void *src1) {
-#if defined(HAVE_MAY_ALIAS)
+static inline int32_t zng_memcmp_4(const void * Z_RESTRICT src0, const void * Z_RESTRICT src1) {
+#if defined(HAVE_MAY_ALIAS) || defined(_MSC_VER)
     return zng_memread_4(src0) != zng_memread_4(src1);
 #else
     return memcmp(src0, src1, 4);
 #endif
 }
 
-static inline int32_t zng_memcmp_8(const void *src0, const void *src1) {
-#if defined(HAVE_MAY_ALIAS)
+static inline int32_t zng_memcmp_8(const void * Z_RESTRICT src0, const void * Z_RESTRICT src1) {
+#if defined(HAVE_MAY_ALIAS) || defined(_MSC_VER)
     return zng_memread_8(src0) != zng_memread_8(src1);
 #else
     return memcmp(src0, src1, 8);
