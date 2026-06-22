e8fa7fa612dab201e7ca3b12c98f7ab380d73499
David Korth <gerbilsoft@gerbilsoft.com>
2026-06-22 18:36:55 -0400

    Extend the MSVC 2015 AVX2/AVX512 workaround to MSVC 2017.
    The 32-bit MSVC 2015 debug build randomly crashes without this because
    the stack ends up not being 16-byte aligned, and MSVC adds a vmovdqa
    instruction.
    
    MSVC 2022 and 2026 align the stack properly before using vmovdqa.

--- arch/x86/chunkset_avx2.c
+++ arch/x86/chunkset_avx2.c
@@ -41,7 +41,7 @@ static inline void chunkmemset_8(uint8_t *from, chunk_t *chunk) {
 
 static inline void chunkmemset_16(uint8_t *from, chunk_t *chunk) {
     /* See explanation in chunkset_avx512.c */
-#if defined(_MSC_VER) && _MSC_VER <= 1900
+#if defined(_MSC_VER) && _MSC_VER < 1920
     halfchunk_t half = _mm_loadu_si128((__m128i*)from);
     *chunk = _mm256_inserti128_si256(_mm256_castsi128_si256(half), half, 1);
 #else
